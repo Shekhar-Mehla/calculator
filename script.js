ff1bc95fa4eec2d8258866fb1037851cb9499e1a
Shekhar Singh <smehla147@gmail.com>
2024-04-28 15:59:18 +1000

    finish the calculator desinging

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("hello world");
+const buttonArray = [
+  "AC",
+  "C",
+  "%",
+  "/",
+  "7",
+  "8",
+  "9",
+  "*",
+  "4",
+  "5",
+  "6",
+  "-",
+  "1",
+  "2",
+  "3",
+  "+",
+  "0",
+  ".",
+  "=",
+];
+buttonArray.map((item, index) => {
+  const maindiv = document.querySelector(".buttons");
+  const node = document.createElement("button");
+  const textnode = document.createTextNode(item);
+  node.appendChild(textnode);
+  maindiv.appendChild(node);
+});
